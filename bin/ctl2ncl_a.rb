48e3ae8e6f8c73619013a71e4ee555ac7c6e767e
Saji Hameed <saji.nh@gmail.com>
2012-04-18 15:26:38 +0900

    some unneeded files deleted

--- bin/ctl2ncl_a.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-require File.join(File.dirname(__FILE__), 'commandline_parser')
-exit
-require File.join(File.dirname(__FILE__), 'grads_utilities')
-
-ctl_to_ncl Choice.choices
--- bin/ctl2ncl_c.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'yaml'
-require File.join(File.dirname(__FILE__), 'highline_parser')
-require File.join(File.dirname(__FILE__), 'grads_utilities')
-
-opts=menu
-ctl_to_ncl opts
